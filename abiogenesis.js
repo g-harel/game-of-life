c5d73c330a9b531fd2a02b669705df665238b1fc
Gabriel <pabombs@gmail.com>
2016-11-16 23:45:28 -0500

    improved gen func and moving to canvas

--- abiogenesis.js
+++ abiogenesis.js
@@ -1,97 +1,48 @@
-// board size
-let size = 42;
 
-// store the cell opacity target for mouseover
+// storing the values to loop around a cell
+let neighbors = [
+    [-1,-1],[-1, 0],[-1, 1],
+    [ 0,-1],/*[00]*/[ 0, 1],
+    [ 1,-1],[ 1, 0],[ 1, 1]
+]
+
+// board size
+let width = 5;
+let height = 5;
+
+// initialize blank board
+let board = (new Array(height)).fill((new Array(width).fill(false)));
+
+// store the target cell opacity for mouseover
 let changeto = 1;
 
-// create cells in the DOM and setup event listener
-window.onload = function() {
-    window.container = document.getElementById('container').children[0];
-    // click event
-    container.addEventListener('mousedown', function(e) {
-        if (e.target.tagName === 'TD' && e.buttons === 1) {
-            e.preventDefault();
-            let source = e.srcElement;
-            changeto = (source.style.opacity < 1)?1:0.01;
-            source.style.opacity = changeto;
-        }
-    });
-    // click + drag event
-    container.addEventListener('mouseover', function(e) {
-        if (e.target.tagName === 'TD' && e.buttons === 1) {
-            e.preventDefault();
-            e.srcElement.style.opacity = changeto;
-        }
-    });
-    let temp = '';
-    for (let i = 0; i < size; i++) {
-        temp += '<tr>';
-        for (let j = 0; j < size; j++) {
-            temp += '<td></td>';
-        }
-        temp += '</tr>';
-    }
-    container.innerHTML = temp;
-    reset();
-};
+// create board
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
 
-// looping the generation animation
-function play() {
-
-}
-
-// stopping the generation loop
-function pause() {
-
-}
-
-// loop over all cells of the DOM
-function iterate(eval) {
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            let cell = container.children[0].children[i].children[j];
-            let updated_val = eval(j, j)?1:0.01;
-            if (cell.style.opacity !== updated_val) {
-                cell.style.opacity = updated_val;
-            }
+// loop over all cells
+function iterate(store, eval) {
+    let temp = store.slice();
+    for (let i = 0; i < height; ++i) {
+        for (let j = 0; j < width; ++j) {
+            temp[i][j] = eval(i, j, store);
         }
     }
+    return temp;
 }
 
-// evaluate the new generation
-function gen() {
-    iterate((i, j) => {
-        let parent = container.children[0];
+// calculate the new generation
+function gen(board) {
+    return iterate(board, (i, j, store) => {
         let count = 0;
-        let right = parent.children[i+1];
-        if (right) {            
-            if (right.children[j+1] && right.children[j+1].style.opacity == 1) { count++; }
-            if (right.children[ j ] && right.children[ j ].style.opacity == 1) { count++; }
-            if (right.children[j-1] && right.children[j-1].style.opacity == 1) { count++; }
-            console.log(right.children[j+1] && right.children[j+1].style.opacity === 1, right.children[j] && right.children[j].style.opacity === 1,right.children[j-1] && right.children[j-1].style.opacity === 1)
+        for (let k = 0; k < neighbors.length; ++k) {
+            count += +container[(height+(neighbors[k][0]+i)%height)%height][(width+(neighbors[k][1]+j)%width)%width];
         }
-        let left = parent.children[i-1];
-        if (left) {            
-            if (left.children[j+1] && left.children[j+1].style.opacity == 1) { count++; }
-            if (left.children[ j ] && left.children[ j ].style.opacity == 1) { count++; }
-            if (left.children[j-1] && left.children[j-1].style.opacity == 1) { count++; }
-        }
-        let middle = parent.children[i];
-        if (middle) {            
-            if (middle.children[j+1] && middle.children[j+1].style.opacity == 1) { count++; }
-            if (middle.children[j-1] && middle.children[j-1].style.opacity == 1) { count++; }
-        }
-        if (count > 0) {
-            console.log('> > ' + count)
-        }
-        //console.log(count)
-        if (parent.children[i].children[j].style.opacity == 1) {
-            if (count < 2) {
-                return false;
-            } else if (count > 3) {
-                return false;
+        if (board[i][j]) {
+            if (count === 2 || count === 3) {
+                return true;
             } else {
-                return true;    
+                return false;
             }
         } else {
             if (count === 3) {
@@ -104,11 +55,11 @@ function gen() {
 }
 
 // randomize board
-function reset() {
-    iterate(() => (Math.random() > 0.9));
+function reset(board) {
+    return iterate(board, () => (Math.random() > 0.9));
 }
 
 // clear the board
-function erase() {
-    iterate(() => false);
+function erase(board) {
+    return iterate(board, () => false);
 }
